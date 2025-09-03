68a3beb6b32c556f0959b020ef2484e4f18620f9
Ken Sternberg <ken@goauthentik.io>
2025-09-03 13:31:33 -0700

    Ported from foundation, and heavily revised.  An analysis of the CSS revealed it was far more complicated than it needed to be; using the Patternfly trick of communicating embedded state through scoped variables names turned out to be much more coherent.

---
+++ src/ak-divider/ak-divider.builder.ts
@@ -0,0 +1,28 @@
+import { IDivider } from "./ak-divider.component.js";
+
+import { html, TemplateResult } from "lit";
+import { ifDefined } from "lit/directives/if-defined.js";
+
+type DividerProps = IDivider & {
+    content?: string | TemplateResult;
+};
+
+/**
+ * @summary Helper function to create a Divider component programmatically
+ *
+ * @returns {TemplateResult} A Lit template result containing the configured ak-divider element
+ *
+ * @see {@link Divider} - The underlying web component
+ */
+export function akDivider(options: DividerProps = {}) {
+    const { variant, orientation, content } = options;
+
+    // Handle string content by wrapping in a span
+    const message = typeof content === "string" ? html`<span>${content.trim()}</span>` : content;
+
+    return html`
+        <ak-divider variant=${ifDefined(variant)} orientation=${ifDefined(orientation)}
+            >${message}</ak-divider
+        >
+    `;
+}
