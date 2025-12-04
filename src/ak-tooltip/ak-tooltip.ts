7a13d4868490eb05e522db527af5b8ce7dc05ca7
Ken Sternberg <ken@goauthentik.io>
2025-12-04 15:22:31 -0800

    Forgot to remove some debugging code.

--- src/ak-tooltip/ak-tooltip.ts
+++ src/ak-tooltip/ak-tooltip.ts
@@ -1,14 +1,17 @@
 import { akTooltip } from "./ak-tooltip.builder.js";
 import { Tooltip } from "./ak-tooltip.component.js";
+
 // window.customElements.define("ak-tooltip", Tooltip);
 // To use the debugging version:
 // ```
-import { TooltipWithHover } from "./ak-tooltip.debug.js";
-
-export { akTooltip, Tooltip };
-window.customElements.define("ak-tooltip", TooltipWithHover(Tooltip));
+// import { TooltipWithHover } from "./ak-tooltip.debug.js";
+// window.customElements.define("ak-tooltip", TooltipWithHover(Tooltip));
 // ```
 //
+
+window.customElements.define("ak-tooltip", Tooltip);
+
+export { akTooltip, Tooltip };
 
 declare global {
     interface HTMLElementTagNameMap {
